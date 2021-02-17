ce11fecd7037363ac1c80eaab103724f2058b4c4
Vilady7 <77541605+Vilady7@users.noreply.github.com>
2021-02-16 19:32:08 -0500

    ok

--- Automatizacion/src/test/java/com/opensource/admin/qa/TC003_Admin_AddNewUser.java
+++ Automatizacion/src/test/java/com/opensource/admin/qa/TC003_Admin_AddNewUser.java
@@ -2,6 +2,15 @@ package com.opensource.admin.qa;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 
 public class TC003_Admin_AddNewUser {
@@ -13,6 +22,39 @@ public class TC003_Admin_AddNewUser {
 
   @Test
   public void TC003_Admin_AddNewUser_TC() {
+	  // STEP 1
+	  Reporter.log("Open Browser \"OrangeHRM\" web page");
+	  System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chrome/chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://opensource-demo.orangehrmlive.com/");
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+	  // STEP 2
+	  Reporter.log("Enter Username and Password");
+	  driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+	  driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
+	  driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	  
+	  // STEP 3
+	  Reporter.log("Validate that you have logged in successfully");
+	  WebDriverWait wait = new WebDriverWait(driver, 30);
+	  wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id = 'menu_admin_viewAdminModule']")));
+
+	  // STEP 4
+	  Reporter.log("Click Admin - Go to the admin page");
+	  driver.findElement(By.xpath("//a[@id = 'menu_admin_viewAdminModule']")).click();
+	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+	  // STEP 5
+	  Reporter.log("Click Add");
+	  driver.findElement(By.xpath("//input[@id = 'btnAdd']")).click();
+	  
+	  // STEP 6
+	  Reporter.log("Enter valid Employee Name");
+	  driver.findElement(By.xpath("//input[@id='systemUser_employeeName_empName']")).click();
+	  
 	  
   }
   
